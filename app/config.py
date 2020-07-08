3b634c3c1b1bc1126c141ab69510f23b0588e46e
Kiran Pachhai <kiran.pachhai@gmail.com>
2020-07-08 15:19:08 -0400

    Prepping for production release

--- app/config.py
+++ app/config.py
@@ -12,39 +12,39 @@ LOG_LEVEL = "DEBUG"
 DEBUG = True
 
 MONGO = {
-    "DATABASE": config('MONGO_DATABASE'),
-    "HOST": config('MONGO_HOST'),
-    "PORT": config('MONGO_PORT'),
-    "USERNAME": config('MONGO_USERNAME'),
-    "PASSWORD": config('MONGO_PASSWORD')
+    "DATABASE": config('MONGO_DATABASE', default="validatordb", cast=str),
+    "HOST": config('MONGO_HOST', default="localhost", cast=str),
+    "PORT": config('MONGO_PORT', default=27019, cast=int),
+    "USERNAME": config('MONGO_USERNAME', default="mongoadmin", cast=str),
+    "PASSWORD": config('MONGO_PASSWORD', default="validatormongo", cast=str)
 }
 
-VOUCH_APIKEY=config('VOUCH_APIKEY')
+VOUCH_APIKEY=config('VOUCH_APIKEY', default="eyvc79BEYdycHYQFhqCKXKsdzqt", cast=str)
 
-JWT_SECRETKEY=config('JWT_SECRETKEY')
+JWT_SECRETKEY=config('JWT_SECRETKEY', default="SECRET_KEY", cast=str)
 
 REDIS = {
-    "HOST": config('REDIS_HOST'),
-    "PORT": config('REDIS_PORT')
+    "HOST": config('REDIS_HOST', default="localhost", cast=str),
+    "PORT": config('REDIS_PORT', default=6379, cast=int)
 }
 
 WALLET = {
     "DID_REQUESTER": "", 
-    "STORE_ROOT": config('WALLET_STORE_ROOT'),
-    "STORE_PASSWORD": config('WALLET_STORE_PASSWORD'),
-    "MNEMONIC": config('WALLET_MNEMONIC'),
+    "STORE_ROOT": config('WALLET_STORE_ROOT', default="didstore", cast=str),
+    "STORE_PASSWORD": config('WALLET_STORE_PASSWORD', default="password", cast=str),
+    "MNEMONIC": config('WALLET_MNEMONIC', default="bean brave rain crush pottery bone lamp purse vintage valley access lawsuit", cast=str),
     "MNEMONIC_PASSPHRASE": ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(100)),
-    "RESOLVE_URL": config('WALLET_RESOLVE_URL'),
-    "CACHE_DIR": config('WALLET_CACHE_DIR')
+    "RESOLVE_URL": config('WALLET_RESOLVE_URL', default="http://api.elastos.io:20606", cast=str),
+    "CACHE_DIR": config('WALLET_CACHE_DIR', default="./.cache.did.elastos", cast=str)
 }
 
 EMAIL = {
     "ATTACHMENT_PATH": "/mnt/c/linux",
-    "SENDER": config('EMAIL_SENDER'),
-    "SMTP_SERVER": config('EMAIL_SMTP_SERVER'),
-    "SMTP_PORT": config('EMAIL_SMTP_PORT'),
-    "SMTP_USERNAME": config('EMAIL_SMTP_USERNAME'),
-    "SMTP_PASSWORD": config('EMAIL_SMTP_PASSWORD'),
+    "SENDER": config('EMAIL_SENDER', default="test@test.com", cast=str),
+    "SMTP_SERVER": config('EMAIL_SMTP_SERVER', default="smtp.example.com", cast=str),
+    "SMTP_PORT": config('EMAIL_SMTP_PORT', default="", cast=str),
+    "SMTP_USERNAME": config('EMAIL_SMTP_USERNAME', default="support@example.com", cast=str),
+    "SMTP_PASSWORD": config('EMAIL_SMTP_PASSWORD', default="password", cast=str),
     "SMTP_TLS": config('EMAIL_SMTP_TLS', default=False, cast=bool),
-    "CALLBACK_URL": config('EMAIL_CALLBACK_URL')
+    "CALLBACK_URL": config('EMAIL_CALLBACK_URL', default="http://localhost:8081/v1/validation/callback", cast=str)
 }
